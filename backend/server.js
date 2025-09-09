b14293877bfdb53aabb06d9150a13d4bb51c6363
aviv <avivfilber.99@gmail.com>
2025-09-09 15:43:37 +0300

    add link

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://crypto-dash-one-theta.vercel.app/login'
+    'https://crypto-dash-one-theta.vercel.app'
   ],
 }));
 
